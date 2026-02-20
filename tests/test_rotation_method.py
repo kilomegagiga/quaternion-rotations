3d05f35d8de2868a55c3b86dd7208c3517960ad4
kermyk <kermyk93@gmail.com>
2026-02-19 18:21:57 -0600

    Prepare for tests of rotation methods.

---
+++ tests/test_rotation_method.py
@@ -0,0 +1,15 @@
+
+class Test_DeflectionBy:
+  def test_zeroRadiansResultsInOriginalVector(self):
+    ## u1 = direction of travel
+    ## u2 = deflectionCalculation(u1, 0)
+    ## assert u2 == u1
+    assert False  ## not yet implemented
+
+  def test_piRadiansResultsInReflectedVector(self):
+    ## u1 = direction of travel
+    ## u2 = deflectionCalculation(u1, Pi)
+    ## assert u2 == -u1 
+    assert False  ## not yet implemented
+
+
